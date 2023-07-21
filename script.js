8c71da5a61a627299c26282ede72c9d6b36cd678
ShaikhAnis30 <109184307+ShaikhAnis30@users.noreply.github.com>
2023-07-21 13:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let obj = {};
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if(!obj[char]) {
+      obj[char] = 1;
+    }else {
+      obj[char]++;
+    }
+  }
+
+  console.log(obj)
+
+  for (let  i = 0; i < str.length; i++) {
+    const char = str[i];
+    if(obj[char] === 1) return char;
+  }
+  return '$';
